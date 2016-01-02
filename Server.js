96f3020d1203009da083065b522e2b4359ea77e2
David Zafrani <d.zafrani21@gmail.com>
2016-01-02 14:59:00 -0500

    steves end point

--- Server.js
+++ Server.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var app = express();
-app.get('/', function (req, res) {
-    res.send('This is a response to a request!');
-    });
-var server = app.listen(3000, function () {
-    var host = server.address() .address;
-    var port = server.address() .port;
 
-    console.log('Example app listesdfasdf', host, port);
-    });
+
+app.get('/', function (req, res) {
+  res.send('This is a response to a request!');
+});
+
+app.get('/steve', function (req, res) {
+  res.send('hello steve');
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listesdfasdf', host, port);
+});
 
